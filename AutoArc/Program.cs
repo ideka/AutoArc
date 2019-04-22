1559084e3c83ef5f180cd378e498e4b8e01af1c4
Gerardo Marset <gammer1994@gmail.com>
2019-04-21 23:58:00 -0300

    Add class to hold remote update information.

--- AutoArc/Program.cs
+++ AutoArc/Program.cs
@@ -1,26 +1,39 @@
-﻿using System;
-using System.Net;
-using System.Security.Cryptography;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Diagnostics;
 using System.Linq;
 
 namespace AutoArc
 {
     public class Program
     {
-        public const string OldSuffix = ".old";
-        public const string ChecksumSuffix = ".md5sum";
-
-        public const string RemotePath = "https://www.deltaconnected.com/arcdps/x64/";
-        public const string LocalPath = "bin64/";
-
         public const string GameExeName = "Gw2-64.exe";
 
         private static void Main(string[] args)
         {
-            Download(RemotePath, LocalPath, "d3d9.dll", true);
-            Download(RemotePath + "buildtemplates/", LocalPath, "d3d9_arcdps_buildtemplates.dll", false);
+            new Remote
+            {
+                Name = "ArcDPS",
+                RemotePath = "https://www.deltaconnected.com/arcdps/x64/d3d9.dll",
+                ChecksumPath = "https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum",
+            }.Update();
+
+            new Remote
+            {
+                Name = "ArcDPS Build Templates",
+                RemotePath = "https://www.deltaconnected.com/arcdps/x64/buildtemplates/d3d9_arcdps_buildtemplates.dll",
+            }.Update();
+
+            new Remote
+            {
+                Name = "ArcDPS Extras",
+                RemotePath = "https://www.deltaconnected.com/arcdps/x64/extras/d3d9_arcdps_extras.dll",
+            }.Update();
+
+            new Remote
+            {
+                Name = "ArcDPS Mechanics",
+                RemotePath = "http://martionlabs.com/wp-content/uploads/d3d9_arcdps_mechanics.dll",
+                ChecksumPath = "http://martionlabs.com/wp-content/uploads/d3d9_arcdps_mechanics.dll.md5sum",
+            }.Update();
 
             if (args.Any())
                 Process.Start(GameExeName, args.Last());
@@ -28,53 +41,5 @@ namespace AutoArc
                 Process.Start(GameExeName);
         }
 
-        private static void Download(string remotePath, string localPath, string name, bool hasChecksum)
-        {
-            Console.WriteLine("Checking for {0} updates...", name);
-
-            {
-                HttpWebResponse resp = (HttpWebResponse)WebRequest.Create(remotePath + name).GetResponse();
-
-                if (File.Exists(localPath + name) && File.GetLastWriteTimeUtc(localPath + name) >= resp.LastModified)
-                {
-                    Console.WriteLine("Up to date ({0}).", resp.LastModified);
-                    return;
-                }
-
-                Console.WriteLine("Downloading update ({0})...", resp.LastModified);
-                resp.Close();
-            }
-
-            if (File.Exists(localPath + name))
-                File.Copy(localPath + name, localPath + name + OldSuffix, true);
-
-            byte[] file;
-            using (WebClient wc = new WebClient())
-            {
-                file = wc.DownloadData(remotePath + name);
-
-                if (hasChecksum)
-                {
-                    using (MD5 md5 = MD5.Create())
-                    {
-                        string checksum = wc.DownloadString(remotePath + name + ChecksumSuffix).Split(' ')[0];
-                        string downloadChecksum = BitConverter.ToString(md5.ComputeHash(file)).Replace("-", "").ToLower();
-                        bool equal = checksum == downloadChecksum;
-                        Console.WriteLine("{0} == {1} ({2})", checksum, downloadChecksum, equal);
-                        if (!equal)
-                        {
-                            Console.ReadKey();
-                            return;
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("No checksum verification.");
-                }
-            }
-
-            File.WriteAllBytes(localPath + name, file);
-        }
     }
 }
---
+++ AutoArc/Remote.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace AutoArc
+{
+    public class Remote
+    {
+        public const string LocalPath = "bin64/";
+        public const string LocalBakPath = "bin64/bak/";
+
+        public string Name { get; set; }
+        public bool BackupOld { get; set; } = true;
+
+        public string RemotePath { get; set; }
+        public string ChecksumPath { get; set; }
+
+        public string LocalName
+        {
+            get => this.localName ?? this.RemotePath.Split('/').Last();
+            set => this.localName = value;
+        }
+
+        private string localName = null;
+
+        public void Update()
+        {
+            Console.WriteLine("Checking for {0} updates...", this.Name);
+
+            string fullLocalPath = LocalPath + this.LocalName;
+
+            using (var resp = (HttpWebResponse)WebRequest.Create(this.RemotePath).GetResponse())
+            {
+                if (File.Exists(fullLocalPath) && File.GetLastWriteTimeUtc(fullLocalPath) >= resp.LastModified)
+                {
+                    Console.WriteLine("Up to date ({0}).\n", resp.LastModified);
+                    return;
+                }
+
+                Console.WriteLine("Downloading update ({0})...", resp.LastModified);
+            }
+
+            if (File.Exists(fullLocalPath))
+            {
+                Directory.CreateDirectory(LocalBakPath);
+                File.Copy(fullLocalPath, LocalBakPath + this.LocalName, true);
+            }
+
+            byte[] file;
+            using (var wc = new WebClient())
+            {
+                file = wc.DownloadData(this.RemotePath);
+
+                if (this.ChecksumPath != null)
+                {
+                    using (var md5 = MD5.Create())
+                    {
+                        string checksum = wc.DownloadString(this.ChecksumPath).Split(' ')[0].Trim();
+                        string downloadChecksum = BitConverter.ToString(md5.ComputeHash(file)).Replace("-", "").ToLower();
+                        bool equal = checksum == downloadChecksum;
+                        Console.WriteLine("{0} == {1} ({2})", checksum, downloadChecksum, equal);
+                        if (!equal)
+                        {
+                            Console.WriteLine();
+                            Console.ReadKey();
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No checksum verification for {0}.", this.Name);
+                }
+            }
+
+            Console.WriteLine();
+            File.WriteAllBytes(fullLocalPath, file);
+        }
+    }
+}
